b505f83d2c79c037882624816f183e700bb08c95
Amalay <84455469+amalay@users.noreply.github.com>
2021-08-23 19:09:42 +0530

    Added

---
+++ learning/day-01/README.md
@@ -0,0 +1,1 @@
+# Terraform Learning - Day 01
---
+++ learning/day-01/main.tf
@@ -0,0 +1,37 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  # name     = "RG-AV-TF01"
+  name     = var.resource_group_name
+  location = "westus2"
+
+  # 3. Added later to check the update functionality
+  tags = {
+        Environment = "Terraform Getting Started"
+        Team = "DevOps"
+    }
+}
+
+# 2. Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    # name                = "avvnet"
+    name                = var.vnet_name
+    address_space       = ["10.0.0.0/16"]    
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+}
